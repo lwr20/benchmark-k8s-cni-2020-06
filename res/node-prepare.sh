c8925545bd387e6caa04ffb1df76b5e7a33c157d
Alexis Ducastel <alexis@infrabuilder.com>
2020-06-04 08:45:05 +0000

    Adding comments in monitoring scripts

--- res/node-prepare.sh
+++ res/node-prepare.sh
@@ -48,6 +48,10 @@ systemctl restart docker
 cat > stats.sh <<'EOF'
 #!/bin/bash
 COUNT=$1
+
+# metrics.log contains one line per second with these data :
+# %cpu-user %cpu-nice %cpu-system %cpu-iowait %cpu-steal memory-used-MB
+
 tail -n$COUNT /home/ubuntu/metrics.log | \
 	awk '{M+=$6;U+=$1;N+=$2;S+=$3;I+=$4;T+=$5} END {printf "%d\t=%.2f+%.2f+%.2f+%.2f+%.2f\n",M/NR,U/NR,N/NR,S/NR,I/NR,T/NR}'
 EOF
@@ -55,8 +59,23 @@ EOF
 cat > monit.sh <<'EOF'
 #!/bin/bash
 
+# Free -m output sample :
+#	              total        used        free      shared  buff/cache   available
+#	Mem:          64316        6544       35949         143       21821       58631
+#	Swap:             0           0           0
+#
+# Sar output sample:
+#	Linux 4.15.0-101-generic (tokyo)         06/03/2020         _x86_64_        (16 CPU)
+#
+#	04:09:29 PM     CPU     %user     %nice   %system   %iowait    %steal     %idle
+#	04:09:30 PM     all     24.45      0.00      4.33      0.00      0.00     71.22
+#	Average:        all     24.45      0.00      4.33      0.00      0.00     71.22
+#
+
 while true
 do
+	# Output will be like one line per second :
+	# %cpu-user %cpu-nice %cpu-system %cpu-iowait %cpu-steal memory-used-MB
 	echo "$(sar 1 1 | grep Average|awk '{print $3" "$4" "$5" "$6" "$7}') $(free -m|grep Mem:|awk '{print $3}') $(date "+%H:%M:%S")" >> /home/ubuntu/metrics.log
 done
 EOF
