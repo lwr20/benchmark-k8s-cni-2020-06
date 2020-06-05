abbf318e9432b09c1665c83426eb064e003541d6
Lancelot Robson <lancelot.robson@gmail.com>
2020-06-05 20:32:48 +0100

    Hack out everything apart from cluster operations

--- bench.sh
+++ bench.sh
@@ -70,40 +70,44 @@ TOT_TCP=0
 for i in $(seq 1 $BENCH_CYCLE)
 do
 	mon_start
-	RES_TCP=$(bench_kubectl bench -it --image=infrabuilder/netbench:client -- iperf3 -c $IP -O $(( $IPERFTIME / 10 )) -f m -t $IPERFTIME 2>/dev/null \
-		| grep receiver| awk '{print $7}')
-	[ "$i" = "1" ] && MON="$(mon_end)"
-	TOT_TCP=$(( TOT_TCP + RES_TCP ))
-	info "TCP $i/$BENCH_CYCLE : $RES_TCP Mbit/s"
+	bench_kubectl bench -it --image=infrabuilder/netbench:client -- iperf3 -c $IP -O $(( $IPERFTIME / 10 )) -f m -t $IPERFTIME
+#	RES_TCP=$(bench_kubectl bench -it --image=infrabuilder/netbench:client -- iperf3 -c $IP -O $(( $IPERFTIME / 10 )) -f m -t $IPERFTIME 2>/dev/null \
+#		| grep receiver| awk '{print $7}')
+#	[ "$i" = "1" ] && MON="$(mon_end)"
+#	TOT_TCP=$(( TOT_TCP + RES_TCP ))
+#	info "TCP $i/$BENCH_CYCLE : $RES_TCP Mbit/s"
+	mon_end
 	sleep 1
 done
-RES_TCP=$(( $TOT_TCP / $BENCH_CYCLE ))
-info "TCP result $RES_TCP Mbit/s"
-SUMMARY="$SUMMARY\t$RES_TCP\t$MON"
+#RES_TCP=$(( $TOT_TCP / $BENCH_CYCLE ))
+#info "TCP result $RES_TCP Mbit/s"
+#SUMMARY="$SUMMARY\t$RES_TCP\t$MON"
 
 #===[ UDP ]=======
 info "Launching benchmark for UDP"
-TOT_UDP=0
-TOT_JIT=0
-TOT_DROP=0
+#TOT_UDP=0
+#TOT_JIT=0
+#TOT_DROP=0
 for i in $(seq 1 $BENCH_CYCLE)
 do
 	mon_start
-	read RES_UDP JITTER_UDP DROP_UDP <<< $(bench_kubectl bench -it --image=infrabuilder/netbench:client -- iperf3 -u -b 0 -c $IP -O $(( $IPERFTIME / 10 )) -w 256K -f m -t $IPERFTIME 2>/dev/null \
-		| grep receiver| sed 's/.* sec//'|awk '{print $3" "$5" "$8}' | tr -d "()%")
-	[ "$i" = "1" ] && MON="$(mon_end)"
-	TOT_UDP=$(( TOT_UDP + RES_UDP ))
-	PART_JIT=$(printf "%.3f" $JITTER_UDP| tr -d "."| sed 's/^0*//')
-	TOT_JIT=$(( TOT_JIT + PART_JIT ))
-	TOT_DROP=$(( TOT_DROP + $( printf "%.0f" $DROP_UDP) ))
-	info "UDP $i/$BENCH_CYCLE : $RES_UDP Mbit/s ${PART_JIT}us jitter ${DROP_UDP}% drop"
+	bench_kubectl bench -it --image=infrabuilder/netbench:client -- iperf3 -u -b 0 -c $IP -O $(( $IPERFTIME / 10 )) -w 256K -f m -t $IPERFTIME 2>/dev/null
+#	read RES_UDP JITTER_UDP DROP_UDP <<< $(bench_kubectl bench -it --image=infrabuilder/netbench:client -- iperf3 -u -b 0 -c $IP -O $(( $IPERFTIME / 10 )) -w 256K -f m -t $IPERFTIME 2>/dev/null \
+#		| grep receiver| sed 's/.* sec//'|awk '{print $3" "$5" "$8}' | tr -d "()%")
+#	[ "$i" = "1" ] && MON="$(mon_end)"
+#	TOT_UDP=$(( TOT_UDP + RES_UDP ))
+#	PART_JIT=$(printf "%.3f" $JITTER_UDP| tr -d "."| sed 's/^0*//')
+#	TOT_JIT=$(( TOT_JIT + PART_JIT ))
+#	TOT_DROP=$(( TOT_DROP + $( printf "%.0f" $DROP_UDP) ))
+#	info "UDP $i/$BENCH_CYCLE : $RES_UDP Mbit/s ${PART_JIT}us jitter ${DROP_UDP}% drop"
+	mon_end
 	sleep 1
 done
-RES_UDP=$(( $TOT_UDP / $BENCH_CYCLE ))
-JIT_UDP=$(( $TOT_JIT / $BENCH_CYCLE ))
-DROP_UDP=$(( $TOT_DROP / $BENCH_CYCLE ))
-info "UDP result $RES_UDP Mbit/s ${JIT_UDP}us jitter ${DROP_UDP}% drop"
-SUMMARY="$SUMMARY\t$RES_UDP\t$JIT_UDP\t$DROP_UDP\t$MON"
+#RES_UDP=$(( $TOT_UDP / $BENCH_CYCLE ))
+#JIT_UDP=$(( $TOT_JIT / $BENCH_CYCLE ))
+#DROP_UDP=$(( $TOT_DROP / $BENCH_CYCLE ))
+#info "UDP result $RES_UDP Mbit/s ${JIT_UDP}us jitter ${DROP_UDP}% drop"
+#SUMMARY="$SUMMARY\t$RES_UDP\t$JIT_UDP\t$DROP_UDP\t$MON"
 
 info "Cleaning resources"
 kubectl delete -f kubernetes/server-iperf3.yml >/dev/null
@@ -125,17 +129,20 @@ TOT_HTTP=0
 for i in $(seq 1 $BENCH_CYCLE)
 do
 	mon_start
-	RES_HTTP=$(bench_kubectl bench -it --image=infrabuilder/netbench:client \
-		-- curl -o /dev/null -skw "%{speed_download}" http://$IP/10G.dat 2>/dev/null| sed 's/\..*//' )
-	[ "$i" = "1" ] && MON="$(mon_end)"
-	TOT_HTTP=$(( $TOT_HTTP + RES_HTTP ))
-	info "HTTP $i/$BENCH_CYCLE : $(( $RES_HTTP * 8 / 1024/ 1024 )) Mbit/s"
+	bench_kubectl bench -it --image=infrabuilder/netbench:client \
+		-- curl -o /dev/null -skw "%{speed_download}" http://$IP/10G.dat 2>/dev/null| sed 's/\..*//'
+#	RES_HTTP=$(bench_kubectl bench -it --image=infrabuilder/netbench:client \
+#		-- curl -o /dev/null -skw "%{speed_download}" http://$IP/10G.dat 2>/dev/null| sed 's/\..*//' )
+#	[ "$i" = "1" ] && MON="$(mon_end)"
+#	TOT_HTTP=$(( $TOT_HTTP + RES_HTTP ))
+#	info "HTTP $i/$BENCH_CYCLE : $(( $RES_HTTP * 8 / 1024/ 1024 )) Mbit/s"
+	mon_end
 	sleep 1
 done
-RES_HTTP=$(( $TOT_HTTP * 8 / $BENCH_CYCLE / 1024 / 1024 ))
+#RES_HTTP=$(( $TOT_HTTP * 8 / $BENCH_CYCLE / 1024 / 1024 ))
 
-info "HTTP result $RES_HTTP Mbit/s"
-SUMMARY="$SUMMARY\t$RES_HTTP\t$MON"
+#info "HTTP result $RES_HTTP Mbit/s"
+#SUMMARY="$SUMMARY\t$RES_HTTP\t$MON"
 
 info "Cleaning resources"
 kubectl delete -f kubernetes/server-http.yml >/dev/null
@@ -158,17 +165,20 @@ TOT_FTP=0
 for i in $(seq 1 $BENCH_CYCLE)
 do
 	mon_start
-	RES_FTP=$(bench_kubectl bench -it --image=infrabuilder/netbench:client \
-		-- curl -o /dev/null -skw "%{speed_download}" ftp://$IP/10G.dat 2>/dev/null| sed 's/\..*//' )
-	[ "$i" = "1" ] && MON="$(mon_end)"
-	TOT_FTP=$(( $TOT_FTP + RES_FTP ))
-	info "FTP $i/$BENCH_CYCLE : $(( $RES_FTP * 8 / 1024/ 1024 )) Mbit/s"
+	bench_kubectl bench -it --image=infrabuilder/netbench:client \
+		-- curl -o /dev/null -skw "%{speed_download}" ftp://$IP/10G.dat 2>/dev/null| sed 's/\..*//'
+#	RES_FTP=$(bench_kubectl bench -it --image=infrabuilder/netbench:client \
+#		-- curl -o /dev/null -skw "%{speed_download}" ftp://$IP/10G.dat 2>/dev/null| sed 's/\..*//' )
+#	[ "$i" = "1" ] && MON="$(mon_end)"
+#	TOT_FTP=$(( $TOT_FTP + RES_FTP ))
+#	info "FTP $i/$BENCH_CYCLE : $(( $RES_FTP * 8 / 1024/ 1024 )) Mbit/s"
+	mon_end
 	sleep 1
 done
-RES_FTP=$(( $TOT_FTP * 8 / $BENCH_CYCLE / 1024 / 1024 ))
+#RES_FTP=$(( $TOT_FTP * 8 / $BENCH_CYCLE / 1024 / 1024 ))
 
-info "FTP result $RES_FTP Mbit/s"
-SUMMARY="$SUMMARY\t$RES_FTP\t$MON"
+#info "FTP result $RES_FTP Mbit/s"
+#SUMMARY="$SUMMARY\t$RES_FTP\t$MON"
 
 info "Cleaning resources"
 kubectl delete -f kubernetes/server-ftp.yml >/dev/null
@@ -191,19 +201,23 @@ TOT_SCP=0
 for i in $(seq 1 $BENCH_CYCLE)
 do
 	mon_start
-	RES_SCP=$(bench_kubectl bench -it --image=infrabuilder/netbench:client \
+	bench_kubectl bench -it --image=infrabuilder/netbench:client \
 		-- sshpass -p root scp  -o UserKnownHostsFile=/dev/null \
-		-o StrictHostKeyChecking=no -v root@$IP:/root/10G.dat ./ 2>/dev/null\
-		| grep "Bytes per second" |sed -e 's/.*received //' -e 's/\..*$//' )
-	[ "$i" = "1" ] && MON="$(mon_end)"
-	TOT_SCP=$(( TOT_SCP + RES_SCP ))
-	info "SCP $i/$BENCH_CYCLE : $(( RES_SCP * 8 / 1024/ 1024 )) Mbit/s"
+		-o StrictHostKeyChecking=no -v root@$IP:/root/10G.dat ./ 2>/dev/null
+#	RES_SCP=$(bench_kubectl bench -it --image=infrabuilder/netbench:client \
+#		-- sshpass -p root scp  -o UserKnownHostsFile=/dev/null \
+#		-o StrictHostKeyChecking=no -v root@$IP:/root/10G.dat ./ 2>/dev/null\
+#		| grep "Bytes per second" |sed -e 's/.*received //' -e 's/\..*$//' )
+#	[ "$i" = "1" ] && MON="$(mon_end)"
+#	TOT_SCP=$(( TOT_SCP + RES_SCP ))
+#	info "SCP $i/$BENCH_CYCLE : $(( RES_SCP * 8 / 1024/ 1024 )) Mbit/s"
+	mon_end
 	sleep 1
 done
-RES_SCP=$(( $TOT_SCP * 8 / $BENCH_CYCLE / 1024 / 1024 ))
+#RES_SCP=$(( $TOT_SCP * 8 / $BENCH_CYCLE / 1024 / 1024 ))
 
-info "SCP result $RES_SCP Mbit/s"
-SUMMARY="$SUMMARY\t$RES_SCP\t$MON"
+#info "SCP result $RES_SCP Mbit/s"
+#SUMMARY="$SUMMARY\t$RES_SCP\t$MON"
 
 info "Cleaning resources"
 kubectl delete -f kubernetes/server-ssh.yml >/dev/null
@@ -214,5 +228,5 @@ kubectl delete -f kubernetes/server-ssh.yml >/dev/null
 #==============================================================================
 echo "========================================================================="
 #echo -e "SUMMARY: $RES_TCP\t$RES_UDP\t$JIT_UDP\t$DROP_UDP\t$RES_HTTP\t$RES_FTP\t$RES_SCP"
-echo -e "SUMMARY: $SUMMARY"
+#echo -e "SUMMARY: $SUMMARY"
 echo "========================================================================="
